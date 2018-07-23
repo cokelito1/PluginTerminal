8bbb2f1ac399d9ffa31f73008a40025736c634fb
Jorge Bravo <cokelitocokelito@gmail.com>
2018-07-23 11:35:51 -0400

    Implemented all endpoints of neko dot life

--- plugins/nekoPlugin/include/NekoPlugin.hpp
+++ plugins/nekoPlugin/include/NekoPlugin.hpp
@@ -19,12 +19,19 @@ private:
 
   void PutCommand(std::pair<std::string, cmd> Command);
   void NSFW(std::vector<std::string> args);
+  void SFW(std::vector<std::string> args);
+  void WHY(std::vector<std::string> args);
 
   std::string PluginName = "Neko Plugin";
   std::string baseUrl = "https://nekos.life/api/v2/";
 
   std::map<std::string, std::string> EndPoints;
+  std::map<std::string, std::string> SFWEndPoints;
+
   std::map<std::string, Neko> NekoFunctions;
+  std::map<std::string, Neko> SFWNekoFunctions;
+
+  std::string GetFromNekoDotLife(std::string Endpoint);
 
   std::string GetHentaiGif();
   std::string GetPussy();
@@ -33,8 +40,52 @@ private:
   std::string GetLesbian();
   std::string GetKumi();
   std::string GetCumsluts();
+  std::string GetClassic();
+  std::string GetBoobs();
+  std::string GetBJ();
+  std::string GetAnal();
+  std::string GetAnalArts();
+  std::string GetYuri();
+  std::string GetTrap();
+  std::string GetTits();
+  std::string GetGirlSoloGif();
+  std::string GetGirlSolo();
+  std::string GetSmallBoobs();
+  std::string GetPussyWankGif();
+  std::string GetPussyArt();
+  std::string GetKemonomimi();
+  std::string GetKitsune();
+  std::string GetKeta();
+  std::string GetHolo();
+  std::string GetHoloEro();
+  std::string GetHentai();
+  std::string GetFutanari();
+  std::string GetFemdom();
+  std::string GetFeetGif();
+  std::string GetEroFeet();
+  std::string GetFeet();
+  std::string GetEro();
+  std::string GetEroKitsune();
+  std::string GetEroNeko();
+  std::string GetEroYuri();
+  std::string GetCumArts();
+  std::string GetBlowJob();
+  std::string GetPussyGif();
 
-  std::string HelpMessage = "NSFW <Option> \nHentaiGif\nPussy\nNekoGif\nNeko\nLesbian\nKumi\nCumslut";
+  std::string GetTickle();
+  std::string GetSlap();
+  std::string GetPoke();
+  std::string GetPat();
+  std::string GetMeow();
+  std::string GetLizard();
+  std::string GetKiss();
+  std::string GetHug();
+  std::string GetFoxGirl();
+  std::string GetFeed();
+  std::string GetCuddle();
+
+  std::string GenerateHelpMessage();
+  std::string GenerateSFWHelpMessage();
 };
 
 extern "C" PluginBase *create() {
